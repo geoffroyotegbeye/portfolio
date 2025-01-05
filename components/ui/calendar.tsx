09bafd9789891f80e06abe36145f19b9ef89558f
Lejeunesage <geoffroyotegbeye@gmail.com>
2025-01-05 21:56:01 +0100

    feat():first commit

--- components/ui/calendar.tsx
+++ components/ui/calendar.tsx
@@ -2,17 +2,27 @@
 
 import * as React from 'react';
 import { ChevronLeft, ChevronRight } from 'lucide-react';
-import { DayPicker } from 'react-day-picker';
+import { DayPicker, DayPickerProps } from 'react-day-picker';
 
 import { cn } from '@/lib/utils';
 import { buttonVariants } from '@/components/ui/button';
 
-export type CalendarProps = React.ComponentProps<typeof DayPicker>;
+interface CustomComponents {
+  IconLeft?: React.ComponentType<any>;
+  IconRight?: React.ComponentType<any>;
+}
+
+export type CalendarProps = DayPickerProps & {
+  components?: CustomComponents;
+  classNames?: Record<string, string>;
+  showOutsideDays?: boolean;
+};
 
 function Calendar({
   className,
   classNames,
   showOutsideDays = true,
+  components = {},
   ...props
 }: CalendarProps) {
   return (
@@ -56,11 +66,13 @@ function Calendar({
       components={{
         IconLeft: ({ ...props }) => <ChevronLeft className="h-4 w-4" />,
         IconRight: ({ ...props }) => <ChevronRight className="h-4 w-4" />,
+        ...components,
       }}
       {...props}
     />
   );
 }
+
 Calendar.displayName = 'Calendar';
 
 export { Calendar };
